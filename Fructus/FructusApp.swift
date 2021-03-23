f5042455ea36e6f777be0b52b96026ddcd581df6
Zubair Nurie <mznurie@msn.com>
2021-03-23 03:18:03 -0400

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Zubair Nurie on 2021-03-23.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
